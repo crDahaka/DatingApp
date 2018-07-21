e095b7f669eb4650817da42d4709dc4d4c7655b4
Kristian Bashev <bkristian7@gmail.com>
2018-07-21 15:10:57 +0300

    Upload project skeleton

---
+++ src/DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿namespace DatingApp.API.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using DatingApp.API.Data;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _context.Values.ToListAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/DatingApp.API/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace DatingApp.API
+{
+    using DatingApp.API.Data;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opt => 
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+                
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
